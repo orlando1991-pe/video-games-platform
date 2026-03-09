1a8def680b24007eb06f4e412b8c5c961b898ac9
orlando <orlando28021@gmail.com>
2026-03-09 22:27:55 +0100

    fixes vistest v3

--- frontend/test/home.test.jsx
+++ frontend/test/home.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react"
+import { test, expect } from "vitest"
 import Home from "../src/pages/Home"
 
 test("renders title", () => {
 
- render(<Home />)
+  render(<Home />)
 
- expect(screen.getByText(/Game Catalog/i)).toBeDefined()
+  expect(screen.getByText(/Game/i)).toBeDefined()
 
 })
